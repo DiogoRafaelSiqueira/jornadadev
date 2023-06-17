f4ba0f4dc4569eb2f007820595716ec2a7d101e1
DiogoRafaelSiqueira <diogo.rafaelsiqueira@gmail.com>
2023-06-17 20:29:47 -0300

    initial commit

---
+++ src/pages/Video.js
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import "./video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://assets.designs.ai/videos/videomaker_sample3.mp4"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
