f9fe97bc571600af5ba9cc2ae9bba1eee11fc8f6
DiogoRafaelSiqueira <diogo.rafaelsiqueira@gmail.com>
2023-06-19 16:36:37 -0300

    Compomete_Sidebar/preparandoUI_paraDB

--- src/App.js
+++ src/App.js
@@ -5,9 +5,25 @@ function App() {
   return (
     <div className="App">
        <div className='app__videos'>
-          <Video/>
-          <Video/>
-          <Video/>
+          <Video
+            likes={100}
+            messages={200}
+            shares={300}
+            name="Diogo"
+            description="omega3"
+            music="storyteller"
+            url="https://assets.designs.ai/videos/videomaker_sample3.mp4"
+          />
+          <Video
+            likes={110}
+            messages={210}
+            shares={310}
+            name="Diogo Rafael"
+            description="Gato interagindo"
+            music="storyteller Singer"
+            url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+          />
+          
 
        </div>
     </div>
